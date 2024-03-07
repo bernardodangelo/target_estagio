e727702311239b7659bd8382beb1aa7f94b67be5
Bernardo D'Angelo <bernardodangelo8@gmail.com>
2024-03-07 14:00:39 -0300

    feat: add atividade4

---
+++ Atividade4.java
@@ -0,0 +1,11 @@
+/*
+
+A primeira coisa que eu faria seria ligar o primeiro interruoptor e esperar alguns minutos deixando-o ligado. Em seguida, eu desligaria o primeiro interruptor, ligaria o segundo e iria para alguma das salas.
+
+Situações:
+
+- A lâmpada da sala está acesa, o segundo interruptor de luz é o correto para essa sala.
+- A lâmpada da sala está apagada, porém, se a lâmpada ainda estiver quente, o primeiro interruptor de luz é o correto para essa sala.
+- A lâmpada da sala está apagada e está muito fria, o terceiro interruptor de luz é o correto para essa sala.
+
+*/
