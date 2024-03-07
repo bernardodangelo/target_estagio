0d00f468e06104a5377d77a1346b4c2703799a60
Bernardo D'Angelo <bernardodangelo8@gmail.com>
2024-03-07 13:30:11 -0300

    feat: add atividade1

---
+++ Atividade1.java
@@ -0,0 +1,17 @@
+
+public class Atividade1 {
+
+	public static void main(String[] args) {
+
+		int i = 13, soma = 0, k = 0;
+
+		while (k < i) {
+			k = k + 1;
+			soma = soma + k;
+		}
+
+		System.out.println(soma);
+		// resultado = 91
+	}
+
+}
